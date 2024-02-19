a7873ecdb5473dd04c6114a1086e98a692146d92
Christopher Pascual <caspascual1293@gmail.com>
2024-02-19 11:01:27 -1000

    Added passwords database

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Passwords } from '../../api/password/Password.js';
 
 /* eslint-disable no-console */
 
@@ -9,10 +10,23 @@ const addData = (data) => {
   Stuffs.collection.insert(data);
 };
 
+const addPasswordData = (data) => {
+  console.log(`  Adding: ${data.name} (${data.owner})`);
+  Passwords.collection.insert(data);
+};
+
 // Initialize the StuffsCollection if empty.
 if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.forEach(data => addData(data));
+  }
+}
+
+// Initialize the PasswordsCollection if empty.
+if (Passwords.collection.find().count() === 0) {
+  if (Meteor.settings.defaultPasswords) {
+    console.log('Creating default data.');
+    Meteor.settings.defaultPasswords.forEach(data => addPasswordData(data));
   }
 }
