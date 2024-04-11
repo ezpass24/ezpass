ae1b7b16e245a79c2913e6c3ff41e09edde050c0
Christopher Pascual <caspascual1293@gmail.com>
2024-04-11 13:52:21 -1000

    Increased testcafe wait times to a ridiculous amount

--- app/tests/signout.page.js
+++ app/tests/signout.page.js
@@ -8,7 +8,7 @@ class SignoutPage {
 
   /** Checks that this page is currently displayed. */
   async isDisplayed(testController) {
-    await testController.expect(this.pageSelector.exists).ok();
+    await testController.wait(150000).expect(this.pageSelector.exists).ok();
   }
 }
 
